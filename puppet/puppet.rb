cb99eae3779dd088c3b60d4cb62ff54691f7c8c3
meh <meh@paranoici.org>
2011-09-26 21:10:08 +0200

    Initial rewrite of the puppet, still has to be finished

---
+++ puppet/puppet.rb
@@ -0,0 +1,150 @@
+#! /usr/bin/env ruby
+# Copyleft meh. [http://meh.paranoid.pk | meh@paranoici.org]
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with This program. If not, see <http://www.gnu.org/licenses/>.
+
+require 'eventmachine'
+require 'evma_httpserver'
+require 'em-http-request'
+
+require 'yaml'
+require 'json'
+require 'socksify/http'
+require 'base64'
+
+require 'ap'
+
+module Torb
+	def self.config (path=nil)
+		return @config unless path
+
+		@config = YAML.parse_file(path).transform
+	end
+
+	def self.proxy
+		whole, host, port = Torb.config['proxy'].match(/^(.*?):(.*?)$/).to_a
+
+		{ :host => host, :port => port.to_i, :type => :socks5 }
+	end
+
+	def self.url
+		"http#{'s' if Torb.config['secure']}://#{Torb.config['master']}"
+	end
+
+	config(ARGV.first || 'config.yml')
+
+	trap 'USR1' do
+		config(ARGV.first || 'config.yml')
+	end
+end
+
+class Handler < EventMachine::Connection
+	include EventMachine::HttpServer
+
+	def post_init
+		super
+
+		no_environment_strings
+	end
+
+	def process_http_request
+		# the http request details are available via the following instance variables:
+		#   @http_protocol
+		#   @http_request_method
+		#   @http_cookie
+		#   @http_if_none_match
+		#   @http_content_type
+		#   @http_path_info
+		#   @http_request_uri
+		#   @http_query_string
+		#   @http_post_content
+		#   @http_headers
+
+		response = EventMachine::DelegatedHttpResponse.new(self)
+
+		if @http_request_method == 'POST'
+			response.status  = 200
+			response.content = 'kay'
+			response.send_response
+
+			return
+		end
+
+		whole, id, rid = @http_request_uri.match(%r{^/(\w+)/(\d+)$}).to_a
+
+		EventMachine::HttpRequest.new("#{Torb.url}/puppet/fetch/request/#{Torb.config['host']}/#{Torb.config['key']}/#{id}/#{rid}").get.tap {|http|
+			http.callback {
+				secure, method, headers, uri, data = begin
+					JSON.parse(http.response)
+				rescue
+					response.status 503
+					response.send_response
+					break
+				end
+
+				ssl     = uri.start_with?('https')
+				service = uri.match(%r{/(.*?).onion})[1]
+
+				EventMachine::HttpRequest.new(uri, { :proxy => Torb.proxy }).send(method.downcase, { :head => headers }).tap {|http|
+					http.headers {|headers|
+						response.status  = http.response_header.status
+						response.headers = headers
+
+						response.headers.delete('Transfer-Encoding')
+
+						if response.headers['Set-Cookie']
+							EventMachine::HttpRequest.new("#{Torb.url}/puppet/cookie/set/#{Torb.config['name']}/#{Torb.config['key']}/#{id}/#{Base64.urlsafe_encode64(response.headers.delete('Set-Cookie'))}").get.tap {|http|
+								http.callback { }
+							}
+						end
+
+						if response.headers['Content-Type'] == 'text/html'
+							http.callback {
+								response.content = http.response.
+									gsub(%r{http://(\w*)\.onion}, "http#{?s if secure}://\\1.#{Torb.config['master']}").
+									gsub(%r{https://(\w*)\.onion}, "http#{?s if secure}://\\1.ssl.#{Torb.config['master']}").
+									gsub(%r{href=['"](.*?)['"]}) {|match|
+										uri = match.match(%r{href=['"](.*?)['"]$})[1]
+
+										if (URI.parse(uri).scheme rescue false)
+											match
+										else
+											"href='http#{?s if secure}://#{service}.#{'ssl.' if ssl}#{Torb.config['master']}/#{uri}'"
+										end
+									}
+
+								response.send_response
+							}
+						else
+							http.stream {|chunk|
+								response.chunk chunk
+								response.send_chunks
+							}
+						end
+					}
+				}
+			}
+		}
+	end
+end
+
+EventMachine.run {
+  EventMachine.epoll
+
+	whole, host, port = Torb.config['host'].match(/^(.*?):(.*?)$/).to_a
+
+  EventMachine.start_server(host, port.to_i, Handler)
+
+  puts "Listening on #{host}:#{port}..."
+}
